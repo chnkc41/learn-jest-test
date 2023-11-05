607eda35628d611ce3a69c0bcafd33784890e030
Cihan Koç <cihankoc41@gmail.comm>
2023-11-05 20:40:32 +0100

    added describe test for button component

---
+++ src/components/button/button.test.js
@@ -0,0 +1,25 @@
+describe('Button Component', () => {
+    describe('group of button test', () => {
+        
+    });
+
+    describe('When variant primary', () => {
+        test('should test1', () => {
+            
+        });
+
+        test('should test2', () => {
+            
+        });
+    });
+
+    describe('When variant secondary', () => {
+        test('should test1', () => {
+            
+        });
+
+        test('should test2', () => {
+            
+        });
+    });
+});
---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({chidren, variant = "primary"}) => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export { Button }
