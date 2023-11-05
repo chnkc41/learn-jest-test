12cdc4fd9bf2b90b6bdfc65cc836dd9523b09ffb
Cihan Koç <cihankoc41@gmail.comm>
2023-11-05 21:13:07 +0100

    added getByDisplayValue

--- src/components/test1/index.jsx
+++ src/components/test1/index.jsx
@@ -3,15 +3,16 @@ import React from "react";
 const Test1 = () => {
   return (
     <>
-    
       {/* getByRole */}
       <button>Test1</button>
       <button>test2</button>
-      {/* getByLabelText - getByPlaceholderText -
+      {/* getByLabelText - 
+        getByPlaceholderText - 
+        getByDisplayValue
        it takes label text then take htmlFor data as reference then check ıf there is any ınput with same id  */}
       <label htmlFor="user-name">User name:</label>
-      <input id="user-name" type="text" placeholder="Search" />
-      {/* getByText- Herhangi bir elementin içerisindeki testi kontrol eder. */}
+      <input id="user-name" type="text" placeholder="Search" value="Computer" />
+      {/* getByText- checks inside of any element text */}
       <div>Welcome to Modern Testing</div>
     </>
   );
--- src/components/test1/test1.test.js
+++ src/components/test1/test1.test.js
@@ -14,7 +14,10 @@ test("renders learn react link", () => {
   //   getByPlaceholderText
   const inputPlaceHolder = screen.getByPlaceholderText("Search");
   expect(inputPlaceHolder).toBeInTheDocument();
-  //   getByPlaceholderText 
+  //   getByPlaceholderText
   const anyElementWithText = screen.getByText("Welcome to Modern Testing");
   expect(anyElementWithText).toBeInTheDocument();
+  //   getByDisplayValue
+  const getByDisplayValue = screen.getByDisplayValue("Computer");
+  expect(getByDisplayValue).toBeInTheDocument();
 });
