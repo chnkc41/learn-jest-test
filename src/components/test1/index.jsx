b951e0052027e50057cb62994df95658c8a89772
Cihan Koç <cihankoc41@gmail.comm>
2023-11-05 20:59:09 +0100

    added getByLabelText

--- src/components/test1/index.jsx
+++ src/components/test1/index.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Test1 = () => {
   return (
-   <>
-    <button>Test1</button>
-    <button>test2</button>
+    <>
+      {/* getByRole */}
+      <button>Test1</button>
+      <button>test2</button>
+      {/* getByLabelText - it takes label text then take htmlFor data as reference then check ıf there is any ınput with same id  */}
+      <label htmlFor="user-name">User name:</label>
+      <input id="user-name" type="text" />
     </>
-  )
-}
+  );
+};
 
-export default Test1
+export default Test1;
--- src/components/test1/test1.test.js
+++ src/components/test1/test1.test.js
@@ -1,10 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import Test1 from './index';
+import { render, screen } from "@testing-library/react";
+import Test1 from "./index";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<Test1 />);
-  const element = screen.getByRole('button', {
-    name: "Test1"
-  }); 
+  //   getByRole
+  const element = screen.getByRole("button", {
+    name: "Test1",
+  });
   expect(element).toBeInTheDocument();
+  //   getByLabelText
+  const labelElement = screen.getByLabelText("User name:");
+  expect(labelElement).toBeInTheDocument();
 });
