6c47da2cc1031ab84b811cf14061f805d80873ce
Cihan Koç <cihankoc41@gmail.comm>
2023-11-05 18:05:27 +0100

    Created first test project.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Learn jest test/i);
+  expect(element).toBeInTheDocument();
 });
